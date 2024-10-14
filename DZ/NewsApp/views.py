4a601a093b1c2acc554105d4a148d3be02a3b9c8
lolanich <aleck.mitiaew@yandex.ru>
2024-10-15 02:11:08 +0300

    first commit

---
+++ DZ/NewsApp/views.py
@@ -0,0 +1,21 @@
+import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-dateCreations'
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.datetime.now()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
